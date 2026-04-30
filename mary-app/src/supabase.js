e40932d14b21fa19528e57e9672640975bae0c46
Deybi Marquez <deybi911@gmail.com>
2026-04-29 18:03:49 -0700

    fix supabase.js

--- mary-app/src/supabase.js
+++ mary-app/src/supabase.js
@@ -1,1 +1,6 @@
-notepad src\supabase.js 
+import { createClient } from '@supabase/supabase-js'
+
+const url = import.meta.env.VITE_SUPABASE_URL
+const key = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(url, key)
