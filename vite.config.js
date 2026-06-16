57bbbb0ab3eedabf3fc6807d8a287d2f01661d3b
Deybi Marquez <deybi911@gmail.com>
2026-06-15 20:56:56 -0700

    packet json 2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,22 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'supabase-vendor': ['@supabase/supabase-js'],
+          'charts': ['recharts'],
+          'excel': ['exceljs', 'xlsx', 'file-saver'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+  },
   server: {
-    historyApiFallback: true,
+    port: 3000,
   }
 })
